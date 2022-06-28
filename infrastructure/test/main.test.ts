efa155aa5c27494368e94794b4d4d24486655647
Damjan Georgievski <damjan@loka.com>
2022-06-28 23:07:03 +0200

    update test

--- infrastructure/test/main.test.ts
+++ infrastructure/test/main.test.ts
@@ -1,10 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { AwsBatchDemoStack} from '../src/stack';
-import 'jest-cdk-snapshot';
+import "jest-cdk-snapshot";
+import * as cdk from "aws-cdk-lib";
+
+import { AwsBatchDemoStack} from "../src/stack";
 
 test("snapshot matches previous state", () => {
   const app = new cdk.App();
-  const stack = new AwsBatchDemoStack(app, 'MyTestStack');
+  const stack = new AwsBatchDemoStack(app, "MyTestStack");
 
   expect(stack).toMatchCdkSnapshot({
     ignoreAssets: true,
