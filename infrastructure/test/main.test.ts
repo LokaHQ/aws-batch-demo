d9fa8ffdaa1483626cf983bac07d31c54f84f5ea
Damjan Georgievski <damjan@loka.com>
2022-10-25 16:12:56 +0200

    try to clean-up docker assets images

--- infrastructure/test/main.test.ts
+++ infrastructure/test/main.test.ts
@@ -8,7 +8,32 @@ test("snapshot matches previous state", () => {
   const app = new cdk.App();
   const stack = new AwsBatchDemoStack(app, "MyTestStack");
   const template = Template.fromStack(stack);
-  const templateJson = template.toJSON();
 
+  const jobDefinitions = template.findResources("AWS::Batch::JobDefinition");
+  cleanUpAssetsJobDefinitions(jobDefinitions);
+  const taskDefinitions = template.findResources("AWS::ECS::TaskDefinition");
+  cleanUpAssetsTaskDefinitions(taskDefinitions);
+
+  const templateJson = template.toJSON();
   expect(templateJson).toMatchSnapshot();
 });
+
+
+/*** in-place mutate to replace the Image asset */
+function cleanUpAssetsJobDefinitions(resources: any) {
+  for (const key of Object.keys(resources)) {
+    const resource = resources[key];
+    resource["Properties"]["ContainerProperties"]["Image"] = "dummy";
+  }
+}
+
+/*** in-place mutate to replace the Image asset */
+function cleanUpAssetsTaskDefinitions(resources: any) {
+  for (const key of Object.keys(resources)) {
+    const resource = resources[key];
+    const containerDefitions = resource["Properties"]["ContainerDefinitions"];
+    for (const container of containerDefitions) {
+      container["Image"] = "dummy";
+    }
+  }
+}
