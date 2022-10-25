68622fc2ac6eb29ed05cbf285cd62f17ee5e1a9f
Damjan Georgievski <damjan@loka.com>
2022-10-25 14:10:49 +0200

    update to jest 29

--- infrastructure/jest.config.js
+++ infrastructure/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  "moduleNameMapper": { "^aws-cdk-lib/.warnings.jsii.js$": "<rootDir>/node_modules/aws-cdk-lib/.warnings.jsii.js" },
 };
