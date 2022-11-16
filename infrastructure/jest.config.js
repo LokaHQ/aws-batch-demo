3a39bc5eed038cbbc9c8417b3fddcd610df5d41b
Damjan Georgievski <92797652+gdamjan-loka@users.noreply.github.com>
2022-11-16 19:22:38 +0100

    add snapshot testing (#3)
    Added Jest snapshot testing. This help when refactoring the code but we don't expect changes in the infrastructure. If the snapshot matches the previous state, then we are certain that no changes will happen in the infrastructure.
    
    Had to work-around some cdk/jest issues. Docker image assets are not reproducible, and had to omit them from the snapshot, so that the snapshot is the same when created on a user pc and on a Github CI.

--- infrastructure/jest.config.js
+++ infrastructure/jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  testEnvironment: "node",
+  roots: ["<rootDir>/test"],
+  testMatch: ["**/*.test.ts"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  // see https://github.com/aws/aws-cdk/issues/20622
+  moduleNameMapper: {
+    "^aws-cdk-lib/.warnings.jsii.js$": "<rootDir>/node_modules/aws-cdk-lib/.warnings.jsii.js",
+  },
 };
--- infrastructure/test/main.test.ts
+++ infrastructure/test/main.test.ts
@@ -1,11 +1,45 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
+/// <reference types="jest" />
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
 
-// example test. To run these tests, uncomment this file along with the
-test('Test if Stack is created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Stack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+import { AwsBatchDemoStack } from "../src/stack";
+
+test("snapshot matches previous state", () => {
+  const app = new cdk.App();
+  const stack = new AwsBatchDemoStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
+
+  const jobDefinitions = template.findResources("AWS::Batch::JobDefinition");
+  cleanUpAssetsJobDefinitions(jobDefinitions);
+  const taskDefinitions = template.findResources("AWS::ECS::TaskDefinition");
+  cleanUpAssetsTaskDefinitions(taskDefinitions);
+
+  const templateJson = template.toJSON();
+  expect(templateJson).toMatchSnapshot();
 });
+
+
+/**
+ * Docker image assets (their sha256 id) are not reproducible, so we have to strip them from the test snapshot.
+ * The following 2 functions will strip (replace really) the image ids, so we can match the snapshot.
+ *
+ */
+
+/*** in-place mutate the resources to strip the Image asset */
+function cleanUpAssetsJobDefinitions(resources: any) {
+  for (const key of Object.keys(resources)) {
+    const resource = resources[key];
+    resource["Properties"]["ContainerProperties"]["Image"] = "dummy";
+  }
+}
+
+/*** in-place mutate the resources to strip the Image asset */
+function cleanUpAssetsTaskDefinitions(resources: any) {
+  for (const key of Object.keys(resources)) {
+    const resource = resources[key];
+    const containerDefitions = resource["Properties"]["ContainerDefinitions"];
+    for (const container of containerDefitions) {
+      container["Image"] = "dummy";
+    }
+  }
+}
