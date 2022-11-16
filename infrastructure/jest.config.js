fcfcaab96d32b54b896bd91e7221ae6070da0bfb
Damjan Georgievski <92797652+gdamjan-loka@users.noreply.github.com>
2022-11-16 18:27:22 +0100

    Update infrastructure/jest.config.js

--- infrastructure/jest.config.js
+++ infrastructure/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+  // see https://github.com/aws/aws-cdk/issues/20622
   moduleNameMapper: {
     "^aws-cdk-lib/.warnings.jsii.js$": "<rootDir>/node_modules/aws-cdk-lib/.warnings.jsii.js",
   },
